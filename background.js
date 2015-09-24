2f39f3dca3f640a34641de6f150953d33a33ce41
Peddz24 <jelle.hoffman@hotmail.com>
2015-09-24 15:56:08 +0200

    nieuwe look

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
 var color = 'url("shuffle7.jpg")';
 
-setInterval(mijnfunctie, 7000);
+setInterval(mijnfunctie, 2000);
 
 function mijnfunctie(){
     var body = document.getElementsByTagName("body")[0];
     var w = window.innerWidth;
     
-    if(w > 800) {
-        body.style.backgroundImage = color;
-        if (color == 'url("shuffle7.jpg")') {
-            color = 'url("shuffle2.jpg")';
+    if (w > 800) {
+        if (color == 'url("fotos/shuffle7.jpg")') {
+            color = 'url("fotos/shuffle2.jpg")';
         } else {
-        color = 'url("shuffle7.jpg")';
+        color = 'url("fotos/shuffle7.jpg")';
         }
+        body.style.backgroundImage = color;
     } else {
        body.style.background = '#2E2E2E'; 
     }
